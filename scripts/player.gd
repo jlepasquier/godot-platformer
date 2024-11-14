daec8821140f6c5f2b584a5b1c680aa833c0d8fd
Julien Lepasquier <julien.lepasquier@gmail.com>
2024-11-14 19:09:13 +0100

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+class_name Player
+extends CharacterBody2D
+
+@onready var movement_animations: AnimatedSprite2D = $AnimatedSprite2D
+#@onready var gun_animations: AnimatedSprite2D = $gun_animations
+
+@onready var movement_state_machine: Node = $StateMachine
+#@onready var gun_state_machine: Node = $gun_state_machine
+@onready var player_move_component = $PlayerMoveComponent
+
+func _ready() -> void:
+	movement_state_machine.init(self, movement_animations, player_move_component)
+	#gun_state_machine.init(self, gun_animations, player_move_component)
+
+func _unhandled_input(event: InputEvent) -> void:
+	movement_state_machine.process_input(event)
+	#gun_state_machine.process_input(event)
+
+func _physics_process(delta: float) -> void:
+	movement_state_machine.process_physics(delta)
+	#gun_state_machine.process_physics(delta)
+
+func _process(delta: float) -> void:
+	movement_state_machine.process_frame(delta)
+	#gun_state_machine.process_frame(delta)
